15fa8ffb46e9e3f5424a933e5da70f82ebce3f31
Svarogichz <87203267+Svarogichz@users.noreply.github.com>
2026-06-14 22:01:02 -0600

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void suma();
+float resta(float, float);
+void multiplicacion();
+float division(float, float);
+
+int main() {
+    printf("Calculadora sencilla\n");
+    return 0;
+}
+
+void suma() {
+}
+
+float resta(float a, float b) {
+    return a - b;
+}
+
+void multiplicacion() {
+}
+
+float division(float a, float b) {
+    return a / b;
+}

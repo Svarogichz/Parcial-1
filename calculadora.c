63c4735064391bc1762f91fc31ffada3a834ec19
Christian Ortega <Christian.zcvz@gmail.com>
2026-06-14 22:17:26 -0600

    Agrega multiplicacion y division

--- calculadora.c
+++ calculadora.c
@@ -6,20 +6,56 @@ void multiplicacion();
 float division(float, float);
 
 int main() {
-    printf("Calculadora sencilla\n");
+
+    float a, b;
+
+    printf("Calculadora sencilla\n\n");
+
+    suma();
+
+    printf("\nIngresa dos numeros para las siguientes operaciones: ");
+    scanf("%f %f", &a, &b);
+
+    printf("Resultado resta: %.2f\n", resta(a, b));
+
+    multiplicacion();
+
+    printf("Resultado division: %.2f\n", division(a, b));
+
     return 0;
 }
 
 void suma() {
+
+    float a, b;
+
+    printf("Ingresa dos numeros para sumar: ");
+    scanf("%f %f", &a, &b);
+
+    printf("Resultado suma: %.2f\n", a + b);
 }
 
 float resta(float a, float b) {
+
     return a - b;
 }
 
 void multiplicacion() {
+
+    float a, b;
+
+    printf("\nIngresa dos numeros para multiplicar: ");
+    scanf("%f %f", &a, &b);
+
+    printf("Resultado multiplicacion: %.2f\n", a * b);
 }
 
 float division(float a, float b) {
+
+    if (b == 0) {
+        printf("Error: no se puede dividir entre cero.\n");
+        return 0;
+    }
+
     return a / b;
 }
